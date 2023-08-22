01f93025c469427f953d3221c329e262489c3888
shige0309 <qqa27yed@chic.ocn.ne.jp>
2023-08-22 23:47:22 +0900

    req.query.secret修正

--- pages/api/revalidate/index.ts
+++ pages/api/revalidate/index.ts
@@ -1,30 +1,27 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   revalidated: boolean
 }
-
 type Msg = {
   message: string
 }
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data | Msg>,
 ) {
-  console.log('revalidating notes page...')
-  if (req.query.key !== process.env.REVALIDATE_SECRET) {
-    return res.status(401).json({ message: 'Invalid key' })
+  console.log('Revalidating notes page...')
+  if (req.query.secret !== process.env.REVALIDATE_SECRET) {
+    return res.status(401).json({ message: 'Your secret is invalid !' })
   }
   let revalidated = false
-
   try {
     await res.revalidate('/notes')
     revalidated = true
-  } catch (error) {
-    console.error(error)
+  } catch (err) {
+    console.log(err)
   }
-
-  res.json({ revalidated })
+  res.json({
+    revalidated,
+  })
 }
